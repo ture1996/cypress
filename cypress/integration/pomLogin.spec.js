72e20ad6feb3a62fcca2c16100055808658dedb5
‘Ime <turanjanin1996@gmail.com>
2021-10-11 21:57:30 +0200

    tenth commit

--- cypress/integration/pomLogin.spec.js
+++ cypress/integration/pomLogin.spec.js
@@ -9,23 +9,39 @@ describe('improved login', () => {
         randomEmail: faker.internet.email(),
         randomPassword: faker.internet.password()
     }
-     let correctEmail = 'y@y.y';
-     let correctPassword = '11111111';
+    let correctEmail = 'y@y.y';
+    let correctPassword = '11111111';
 
-     beforeEach("visit link", () => {
-         cy.visit('/');
-         cy.url().should('contains', 'https://gallery-app');
-     });
-    
-    
+    beforeEach("visit link", () => {
+        cy.visit('/');
+        cy.url().should('contains', 'https://gallery-app');
+    });
+
+
     it('login with valid credentials', () => {
+        cy.intercept(
+            "POST",
+            "https://gallery-api.vivifyideas.com/api/auth/login",
+            ).as("validLogin");
         loginPage.login(correctEmail, correctPassword);
+        cy.wait('@validLogin').then((interception) => {
+            console.log(interception.response);
+            expect(interception.response.statusCode).eq(200);
+        });
+
         loginPage.logoutButton.should('be.visible');
     });
-    it('logout', ()=>{
+    it('logout', () => {
+        cy.intercept(
+            "POST",
+            "https://gallery-api.vivifyideas.com/api/auth/logout",
+            ).as("logout");
         loginPage.login(correctEmail, correctPassword);
         loginPage.logoutButton.should('be.visible');
         loginPage.logoutButton.click();
+        cy.wait('@logout').then((interception) => {expect(interception.response.body.message).eq('Successfully logged out');
+        expect(interception.response.statusCode).eq(200);
+    })
     });
     it('login with invalid data', () => {
         loginPage.login(userData.randomEmail, userData.randomPassword);
